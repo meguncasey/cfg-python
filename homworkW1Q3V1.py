018b222c77ac9f539403d446f8da5dbeb76ca7e0
meguncasey <meganleo@live.co.uk>
2024-04-11 09:39:12 +0100

    scran

---
+++ homworkW1Q3V1.py
@@ -0,0 +1,12 @@
+def calculate_omelettes(boxes_of_eggs, eggs_per_box=6, eggs_per_omelette=4):
+    total_eggs = boxes_of_eggs * eggs_per_box
+    omelettes = total_eggs // eggs_per_omelette
+    return omelettes
+
+def main():
+    boxes_of_eggs = int(input("Enter the number of boxes of eggs: "))
+    omelettes = calculate_omelettes(boxes_of_eggs)
+    print(f"You can make {omelettes} omelettes with {boxes_of_eggs} boxes of eggs.")
+
+if __name__ == "__main__":
+    main()
